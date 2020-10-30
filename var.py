1af24dcf47bcd55a632863108aab8a07c2b060ec
Anna Ponyavina <ponyavina1997@mail.ru>
2020-10-30 15:54:16 +0300

    create: lesson02

---
+++ var.py
@@ -0,0 +1,52 @@
+# lesson02
+
+a = 0; print(a, type(a))
+a_second = -5000; print(a_second, type(a_second))
+a_third = 70345; print(a_third, type(a_third))
+
+b = 0.5; print(b, type(b))
+b_second = -48.39; print(b_second, type(b_second))
+b_third = 7.0345; print(b_third, type(b_third))
+
+c = '0'; print(c, type(c))
+c_second = 'python'; print(c_second, type(c_second))
+c_third = "Hello world!"; print(c_third, type(c_third))
+
+d = ('a', 'b', 'c'); print(d, type(d))
+d_second = ('a', {'name': 'Igor'}, False, -5000, ['elem']); print(d_second, type(d_second))
+d_third = (70345, {'name': 'Igor'}); print(d_third, type(d_third))
+
+e = ['c', 'п', 'и', 'с', 'о', 'к']; print(e, type(e))
+e_second = ['todo', '-', [list], 2, True]; print(e_second, type(e_second))
+e_third = ['todo', '-', 'list',]; print(e_third, type(e_third))
+
+f = {'a', 'b', 'c'}; print(f, type(f))
+f_second = {'foot+', 'ball=', 'football'}; print(f_second, type(f_second))
+f_third = {23, 'years'}; print(f_third, type(f_third))
+
+g = {'name': 'Igor', 'children': {'Alisa': 12, 'Pavel': 18}}; print(g, type(g))
+g_second = {'name': 'Alex', 'married': False}; print(g_second, type(g_second))
+g_third = {'70345': 70345}; print(g_third, type(g_third))
+
+h = True; print(h, type(h))
+h_second = False; print(h_second, type(h_second))
+
+a = 3
+b = 5
+с = 7
+
+print(a+a_second)
+print(a-a_second)
+print(a*a_second)
+print(a/a_second)
+print(a//a_second)
+print(a % a_second)
+print(a**3)
+print(a_second+a_third)
+print(a_second-a_third)
+print(a_second*a_third)
+print(a_second/a_third)
+print(a_second//a_third)
+print(a_second % a_third)
+print(a_second ** 0.5)
+
